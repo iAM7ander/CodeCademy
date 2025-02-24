f3ac1c5ab2c0f36d90ec2a56f040fc58ef3fb429
Alexander Plaut <iAM7ander@outlook.com>
2025-02-24 18:31:05 -0500

    new (setTimeout function) added index.js

---
+++ setTimeout-function/index.js
@@ -0,0 +1,13 @@
+// //setTimeout function
+setTimeout(() => {
+	console.log("Delay the printing of this string, please!");
+}, 1000);
+
+//log to verify
+console.log("Doing important stuff.");
+console.log("Still doing important stuff.");
+
+// //setInterval function
+// setInterval(() => {
+// 	alert("Are you paying attention?!?");
+// }, 3000);
